cf0d642b4148842de1c0862dc6a67528362a21b4
Sergey.M <msi1974msi@mail.ru>
2022-02-22 00:12:18 +0300

    first version add

---
+++ src/main/java/ru/home/vtb/operasales/operasalesapp/OperasalesAppApplication.java
@@ -0,0 +1,71 @@
+package ru.home.vtb.operasales.operasalesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.home.vtb.operasales.operasalesapp.services.OperaService;
+
+@SpringBootApplication
+public class OperasalesAppApplication {
+
+    public static void main(String[] args) {
+
+       final ConfigurableApplicationContext ctx = SpringApplication.run(OperasalesAppApplication.class, args);
+
+       // сформируем список релизов
+       ctx.getBean(OperaService.class).newOpera("Борис Годунов", "05.05.2022","16+", "Душераздирающая история про русского царя.");
+       ctx.getBean(OperaService.class).newOpera("Отелло", "06.05.2022","16+", "Спойлер. Мавр таки убийца.");
+       ctx.getBean(OperaService.class).newOpera("Юнона и Авось", "07.05.2022","12+", "Красивая история про верность и любовь в период великого путешествия.");
+       System.out.println(ctx.getBean(OperaService.class).toString());
+
+
+       // продадим пару билетов на Отелло 1ряд, места 4 и 5
+       // отобразим карту зала
+       // для действий с билетами можно контекст присвоить переменной и работать уже с ней - так вроде проще
+       System.out.println(ctx.getBean(OperaService.class).getOpera("Отелло", "06.05.2022").toString());
+       // резервируем билеты
+       ctx.getBean(OperaService.class).getOpera("Отелло", "06.05.2022").saleTiket(1,4);
+       ctx.getBean(OperaService.class).getOpera("Отелло", "06.05.2022").saleTiket(1,5);
+       // отобразим карту зала
+       System.out.println(ctx.getBean(OperaService.class).getOpera("Отелло", "06.05.2022").toString());
+
+       // продадим пару билетов на Борис Годунов 5 ряд, места 6 и 7
+       System.out.println(ctx.getBean(OperaService.class).getOpera("Борис Годунов", "05.05.2022").toString());
+       ctx.getBean(OperaService.class).getOpera("Борис Годунов", "05.05.2022").saleTiket(5,6);
+       ctx.getBean(OperaService.class).getOpera("Борис Годунов", "05.05.2022").saleTiket(5,7);
+       System.out.println(ctx.getBean(OperaService.class).getOpera("Борис Годунов", "05.05.2022").toString());
+
+       // статус по билетам
+       System.out.println(ctx.getBean(OperaService.class).toString());
+
+       // сдадим один билет на Отелло 1х4
+       System.out.println("сдадим один билет на Отелло 1х4");
+       ctx.getBean(OperaService.class).getOpera("Отелло", "06.05.2022").dropTiket(1,4);
+
+       // статус по билетам
+       System.out.println(ctx.getBean(OperaService.class).toString());
+
+       //УПС! Борис меняет дату релиза!
+       System.out.println("\nУПС! Борис меняет дату релиза!");
+       ctx.getBean(OperaService.class).changeOpera("Борис Годунов",
+               "05.05.2022",
+               "Борис Годунов",
+               "15.05.2022",
+               "16+",
+               "Душераздирающая история про русского царя."
+               );
+
+       // статус по билетам
+       System.out.println(ctx.getBean(OperaService.class).toString());
+
+       //УПС! Отелло отменили!
+       System.out.println("\nУПС! Отелло отменили!");
+       ctx.getBean(OperaService.class).dropOpera("Отелло", "06.05.2022");
+
+       // статус по билетам
+       System.out.println(ctx.getBean(OperaService.class).toString());
+
+    }
+
+
+}
---
+++ src/main/java/ru/home/vtb/operasales/operasalesapp/services/OperaService.java
@@ -0,0 +1,75 @@
+package ru.home.vtb.operasales.operasalesapp.services;
+
+import org.springframework.stereotype.Service;
+import ru.home.vtb.operasales.operasalesapp.operas.Opera;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+@Service
+public class OperaService {
+    Map<String, Opera> mapOpera = new HashMap<>(); // key = name + " " + sDate
+    Opera nOpera;
+
+    public Opera getOpera(String name, String sDate){
+        //System.out.println("Создаём новый релиз: "+ name + " " + sDate);
+        nOpera = mapOpera.get(name + " " + sDate);
+        if (nOpera == null) {
+            System.out.println("Такого релиза нет!"+ name + " " + sDate);
+        }
+        return nOpera;
+    }
+
+    public Opera newOpera(String name, String sDate, String category, String description){
+        //System.out.println("Создаём новый релиз: "+ name + " " + sDate);
+        nOpera = mapOpera.get(name + " " + sDate);
+        if (nOpera == null) {
+            System.out.println("Создан новый релиз: "+ name + " " + sDate);
+            nOpera = new Opera(name, sDate, category, description);
+            mapOpera.put(name + " " + sDate,nOpera);
+        }
+        return nOpera;
+    }
+
+    public void dropOpera(String name, String sDate){
+        String key = name + " " + sDate;
+        nOpera = mapOpera.get(key);
+        if (nOpera == null) {
+            System.out.println("Такого релиза нет!" + name + " " + sDate);
+        }  else {
+            mapOpera.remove(key);
+        }
+    }
+
+    public Opera changeOpera(String name, String sDate, String newName, String newDate, String newCategory, String newDescription){
+        String key = name + " " + sDate;
+        String newKey = newName + " " + newDate;
+        nOpera = mapOpera.get(key);
+        if (nOpera == null) {
+            System.out.println("Такого релиза нет!"+ name + " " + sDate);
+        } else {
+            mapOpera.remove(key);
+            mapOpera.put(newKey, nOpera);
+            nOpera.changeOpera(newName, newDate, newCategory,newDescription);
+        }
+        return nOpera;
+    }
+
+    @Override
+    public String toString(){
+        Iterator<Map.Entry<String, Opera>> itr = mapOpera.entrySet().iterator();
+        StringBuilder sOperas = new StringBuilder();
+        System.out.println("Предлагаем вам список релизов:\n");
+        while (itr.hasNext()) {
+            Map.Entry<String, Opera> entry =  itr.next();
+            String key = entry.getKey();
+            Opera opera = entry.getValue();
+            sOperas.append(key + ".\n Количество свободных билетов = " + opera.countfreetikets() + "\n");
+            sOperas.append("Возрастная категоря:" + opera.getCategory() + "\n");
+            sOperas.append("Описание: " + opera.getDescription() + "\n");
+            sOperas.append("----------------------------------------\n");
+        }
+        return String.valueOf(sOperas);
+    }
+}
